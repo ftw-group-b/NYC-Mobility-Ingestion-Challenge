04bfd97426340d23b24c2d111f0761e43f2f7f59
samchvs <chavessamantha019@gmail.com>
2026-09-19 13:17:10 +0800

    new test

--- src/04_gold/05_fact_green_taxi_trip.sql
+++ src/04_gold/05_fact_green_taxi_trip.sql
@@ -57,7 +57,7 @@ SELECT
     CASE
         WHEN g.trip_distance > 0
         THEN g.total_amount / g.trip_distance
-    END AS total_amount_per_mile,
+    END AS total_amount_per_mile, --new
 
     g.fare_amount,
     g.extra,
